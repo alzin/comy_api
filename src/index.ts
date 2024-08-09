91636e80259de50190f5a0047e03fd5953c60b25
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2024-08-09 14:42:34 +0900

    add cors to accept all requests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { log } from "console";
 import express from "express";
+import cors from "cors";
 import authRouter from "./routes/auth";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRouter);
 
