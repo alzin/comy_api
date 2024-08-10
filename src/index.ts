546c61ce3b57ca62062de252218c83db08c52c7e
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2024-08-11 00:25:59 +0900

    use swagger-jsdoc library to solve the problem

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,31 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth";
 import { serve, setup } from "swagger-ui-express";
+import swaggerJSDocs from "swagger-jsdoc";
+import swaggerDocument from "./swagger.json";
 import dotenv from "dotenv";
 dotenv.config();
 
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
 const PORT = process.env.PORT || 3000;
+
+const definition = swaggerDocument;
+
+const options = {
+  definition,
+  apis: ["./index.ts"],
+};
+
+const swaggerSpec = swaggerJSDocs(options);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
 
-app.use(
-  "/docs",
-  serve,
-  setup(undefined, {
-    customCssUrl: CSS_URL,
-    swaggerOptions: {
-      url: "/swagger.json",
-    },
-  }),
-);
+app.get("/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+app.use("/docs", serve, setup(swaggerSpec));
 
 app.use("/auth", authRouter);
 
