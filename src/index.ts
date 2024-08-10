92d437b49402fd821ae0a6196cd8b4d542dc178b
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2024-08-10 23:51:12 +0900

    fix swagger document now showing on vercel by serving the swagger.json as a static url saved in public directory

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,32 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth";
 import { serve, setup } from "swagger-ui-express";
-import swaggerDocument from './swagger.json';
 import dotenv from "dotenv";
 dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', serve, setup(swaggerDocument));
-app.use('/auth', authRouter);
+app.use(express.static("public"))
 
-const PORT = process.env.PORT || 3000;
+app.use(
+  "/docs",
+  serve,
+  setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  }),
+);
 
-app.get('/', (req, res) => {
-    res.status(200).send('OK');
+app.use("/auth", authRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
 });
 
 app.listen(PORT, () => {
-    log(`Server is running on http://localhost:${PORT}`);
+  log(`Server is running on http://localhost:${PORT}`);
 });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import User from '../models/user';
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
 import dotenv from "dotenv";
-import { log } from 'console';
+import { log } from "console";
 dotenv.config();
 
 const jwtSecret = process.env.JWT_SECRET;
 if (!jwtSecret) {
-    throw new Error("JWT_SECRET is not defined");
+  throw new Error("JWT_SECRET is not defined");
 }
 
 const router = express.Router();
@@ -16,28 +16,28 @@ const router = express.Router();
 // Mock database
 const users: User[] = [];
 
-router.post('/register', async(req, res) => {
-    const { email, name, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = { email, name, password: hashedPassword}
-    users.push(user);
-    log(users);
+router.post("/register", async (req, res) => {
+  const { email, name, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = { email, name, password: hashedPassword };
+  users.push(user);
+  log(users);
 
-    res.status(201).json({ message: 'User registered successfully' });
+  res.status(201).json({ message: "User registered successfully" });
 });
 
-router.post('/login', async(req, res) => {
-    const { email, password } = req.body;
-    const user = users.find(user => user.email === email);
-    if (!user) {
-        return res.status(400).json({ message: 'Invalid username or password' });
-    }
-    const isPsswordValid = await bcrypt.compare(password, user.password);
-    if (!isPsswordValid) {
-        return res.status(400).json( { message: 'Invalid username or password' } );
-    }
-    const token = jwt.sign({ id: user.email }, jwtSecret, { expiresIn: '1h' });
-    res.json( { token } );
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (!user) {
+    return res.status(400).json({ message: "Invalid username or password" });
+  }
+  const isPsswordValid = await bcrypt.compare(password, user.password);
+  if (!isPsswordValid) {
+    return res.status(400).json({ message: "Invalid username or password" });
+  }
+  const token = jwt.sign({ id: user.email }, jwtSecret, { expiresIn: "1h" });
+  res.json({ token });
 });
 
 export default router;
