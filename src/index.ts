0efa12536ab77bb3957d071cb0c84702d5171afd
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2024-08-10 23:10:20 +0900

    add swagger ui
    add /auth/register
    
    add /auth/login

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import { log } from "console";
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth";
+import { serve, setup } from "swagger-ui-express";
+import swaggerDocument from './swagger.json';
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', serve, setup(swaggerDocument));
 app.use('/auth', authRouter);
 
 const PORT = process.env.PORT || 3000;
