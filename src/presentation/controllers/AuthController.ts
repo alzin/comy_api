9ab53e22b308f3db940504a40b1f357f7dfffbc5
Mohamad Ghaith Alzin <ghaith@Mohamads-MacBook-Pro.local>
2024-09-10 22:15:33 +0900

    change the accessToken for 1h and the refreshToken for 7d

--- src/presentation/controllers/AuthController.ts
+++ src/presentation/controllers/AuthController.ts
@@ -11,7 +11,10 @@ export class AuthController {
       httpOnly: true,
       secure: CONFIG.NODE_ENV === "production",
       sameSite: "none",
-      maxAge: name === CONFIG.REFRESH_TOKEN_COOKIE_NAME ? 30 * 1000 : 15 * 1000, //  60 seconds for refresh, 10 seconds for access
+      maxAge:
+        name === CONFIG.REFRESH_TOKEN_COOKIE_NAME
+          ? 7 * 24 * 60 * 60 * 1000
+          : 60 * 60 * 1000, // 7 days for refresh, 1 hour for access
     });
   }
 
