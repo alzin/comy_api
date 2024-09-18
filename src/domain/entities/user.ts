1245180e30937b606c4b90239943105a709829b6
Mohamad Ghaith Alzin <ghaith@Mohamads-MacBook-Pro.local>
2024-09-18 23:16:49 +0900

    add category to User data

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -4,6 +4,7 @@ export interface User {
   id?: string;
   email: string;
   name: string;
+  category: string;
   password: string;
   isEmailVerified: boolean;
   verificationToken?: string | null;
