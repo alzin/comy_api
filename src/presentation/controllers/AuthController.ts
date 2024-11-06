ba51d816f7045ff192d631a8878e31eee21326d9
obedah shaban <95497072+obedah96@users.noreply.github.com>
2024-11-06 11:03:19 -0800

    fix conflicts

--- src/presentation/controllers/AuthController.ts
+++ src/presentation/controllers/AuthController.ts
@@ -171,8 +171,8 @@ export class AuthController {
       const cookieOptions = {
         httpOnly: true,
         secure: CONFIG.NODE_ENV === "production",
-        sameSite: "none" as const,
-      };
+        sameSite: "none" as const
+      }
       res.clearCookie(CONFIG.ACCESS_TOKEN_COOKIE_NAME, cookieOptions);
       res.clearCookie(CONFIG.REFRESH_TOKEN_COOKIE_NAME, cookieOptions);
       res.status(200).json({ message: "Logged out successfully" });
