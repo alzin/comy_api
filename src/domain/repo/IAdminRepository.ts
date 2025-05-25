8f650a6ac0748f85207ff63251d5cfe4358221ed
Lubna <lubna@Lubnas-MacBook-Air.local>
2025-05-25 11:07:00 +0300

    remove admain

--- src/domain/repo/IAdminRepository.ts
+++
@@ -1,5 +0,0 @@
-import {Admin} from "../entities/Admin";
-export interface IAdminRepository{
-    create(admin:Admin):Promise<Admin>;
-    login(name:string,password:string):Promise<Admin | null>;
-}
