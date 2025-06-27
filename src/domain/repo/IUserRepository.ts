ca0e75b666e1e19d7f18d3990a21428c3f3295a5
Lubna <lubna@Lubnas-MacBook-Air.local>
2025-06-27 13:47:58 +0300

    Delete duplicate update function and correct create function

--- src/domain/repo/IUserRepository.ts
+++ src/domain/repo/IUserRepository.ts
@@ -3,14 +3,13 @@ import { User } from '../entities/User';
 import { UserInfo } from '../entities/UserInfo';
 
 export interface IUserRepository {
-  create(user: Omit<User, 'id'>): Promise<User>;
+  create(user: User): Promise<User>;
   findById(id: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
   findByVerificationToken(token: string): Promise<User | null>;
   findByStripeCustomerId(stripeCustomerId: string): Promise<User | null>;
   findActiveUsers(): Promise<User[]>;
   getAllUsersInfo(): Promise<UserInfo[]>;
-  update(id: string, update: Partial<User>): Promise<User | null>;
   update(userId: string, userData: Partial<User>): Promise<void>;
   updateUserStatus(userId: string, isOnline: boolean): Promise<boolean>;
   searchUsers(searchTerm: string): Promise<UserInfo[]>;
